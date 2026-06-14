e083ccac951d8084569c24334ecdfd7fd67a8d44
Tanvir-Sheikh-R <tanvirsheikh0177@gmail.com>
2026-06-14 20:13:27 +0600

    login page

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from services.auth.login_wall import render_login_wall
 
-st.set_page_config(page_title="AI GYM Coach")
 
-st.title("🏋️ AI Real-time GYM Coach")
-st.write("Setup successful!")
+
+def main():
+    st.set_page_config(
+        page_icon="🏋️‍♂️",
+        page_title= 'FitCoach-AI',
+        initial_sidebar_state='expanded',
+        layout='centered'
+    )
+
+    if not render_login_wall():
+        return
+    
+    st.write('Hello '+ st.session_state['username'])
+    
+if __name__ == "__main__":
+    main()
---
+++ services/auth/login_wall.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+def render_login_wall():
+    if st.session_state.get('user_id'):
+        return True
+    
+    st.title("🏋️‍♂️ AI Real_time GYM Trainer")
+    st.markdown('### Welcome! Please enter a username to start.')
+
+    with st.form('login_form', clear_on_submit=False):
+        username = st.text_input('Name (unique)', placeholder='unique name e.g. tanvir')
+        submit_button = st.form_submit_button('Start Session', width='stretch', type='primary')
+
+
+    if submit_button:
+        if not username:
+            st.error('Name cannot be empty')
+            return False
+        
+        st.session_state['username'] = username
+        st.session_state['user_id'] = '1'
+        st.rerun()
+
+
+
+
+
+
+
+
+
+    return False
