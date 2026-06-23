8f22e07c34d4e306dfca540a0fa7dd79e25dafd3
Tanvir-Sheikh-R <tanvirsheikh0177@gmail.com>
2026-06-23 23:09:02 +0600

    database featurea

---
+++ services/persistence/Exercise_repository.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import sqlite3
+from pathlib import Path
+
+_DB_PATH =str(Path(__file__).parent.parent.parent / 'data.db')
+
+def _get_connection():
+    conn = sqlite3.connect(_DB_PATH, check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = _get_connection()
+
+    with conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS user(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP       
+            )
+        """)
+
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS exersises(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL REFFERENCES user(id),
+                exercise_name TEXT NOT NULL,
+                reps INTEGER NOT NULL DEFAULT 0,
+                sets INTEGER NOT NULL DEFAULT 0,
+                time INTEGER NOT NULL DEFAULT 0,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP         
+            )
+        """)
+
+def get_user(username: str) -> sqlite3.Row:
+    conn = _get_connection()
+
+    return conn.execute("""
+                SELECT * FROM users WHERE USERNAME = ?
+            """, (username))
+
+def create_user(username: str) -> sqlite3.Row:
+    conn = _get_connection()
+
+    with conn:
+        conn.execute("""
+           INSERT INTO users (username) VALUES (?)
+        """, (username))
+
+    return get_user(username)
+
+def get_or_create_user(username: str) -> sqlite3.Row:
+    user = get_user(username)
+
+    if user is None:
+        user = create_user(username)
+
+    return user
+
